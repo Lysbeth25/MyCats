0daeb4ceb6608ff3a6a92a487a809a2bad36612f
Lysbeth25 <147013266+Lysbeth25@users.noreply.github.com>
2023-10-05 12:17:49 +0800

    MyCats

---
+++ script.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const learnButtons = document.querySelectorAll(".learn-more");
+
+    learnButtons.forEach((button) => {
+        button.addEventListener("click", function () {
+            const targetId = button.getAttribute("data-toggle");
+            const targetList = document.getElementById(targetId);
+
+            if (targetList.style.display === "none" || targetList.style.display === "") {
+                targetList.style.display = "block";
+            } else {
+                targetList.style.display = "none";
+            }
+        });
+    });
+});
